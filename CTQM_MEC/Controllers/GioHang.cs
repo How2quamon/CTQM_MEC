29ac4b398368e4240e91d35851c03523bb4022ca
Nghia2510 <90500652+Nghia2510@users.noreply.github.com>
2022-12-18 11:54:28 +0700

    add cart

---
+++ CTQM_MEC/Controllers/GioHang.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CTQM_MEC.Controllers
+{
+    public class GioHang : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
