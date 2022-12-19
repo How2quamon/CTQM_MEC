b2c4dd43154ee639fb3dba3e5c935f1b5b60b7ed
DucNhat <nguyenducnhat142@gmail.com>
2022-12-19 12:29:22 +0700

    paypal

--- CTQM_MEC/Controllers/HoaDonsController.cs
+++ CTQM_MEC/Controllers/HoaDonsController.cs
@@ -7,16 +7,129 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CTQM_Car.Data;
 using CTQM_MEC.Data;
+using System.Security.Cryptography.X509Certificates;
+using PayPal.Core;
+using PayPal.v1.Payments;
+using BraintreeHttp;
 
 namespace CTQM_MEC.Controllers
 {
     public class HoaDonsController : Controller
     {
         private readonly CTQMDbContext _context;
+        private readonly string _clientId;
+        private readonly string _secretKey;
 
-        public HoaDonsController(CTQMDbContext context)
+
+        public double TyGia = 23300;
+        public HoaDonsController(CTQMDbContext context, IConfiguration config)
         {
             _context = context;
+            _clientId = config["PaypalSettings:ClientId"];
+            _secretKey = config["PaypalSettings:SecretKey"];
+        }
+
+        public async Task<IActionResult> PaypalCheckout()
+        {
+            var environment = new SandboxEnvironment(_clientId, _secretKey);
+            var client = new PayPalHttpClient(environment);
+
+            #region Create Paypal Order
+            var itemList = new ItemList()
+            {
+                Items = new List<Item>()
+            };
+            var total = Math.Round(_context.HoaDons.Sum(p => p.ThanhTien)/ TyGia, 2);
+            foreach (var item in _context.HoaDons)
+            {
+                itemList.Items.Add(new Item()
+                {
+                    Name = item.MaGiaoDich.ToString(),
+                    Currency = "USD",
+                    Price = Math.Round(item.ThanhTien/TyGia,2).ToString(),
+                    //Quantity = item.Quantity.ToString(),
+                    Sku = "sku",
+                    Tax = "0"
+                });
+            }
+            #endregion
+            var paypalOrderId = DateTime.Now.Ticks;
+            var hostName = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+            var payment = new Payment()
+            {
+                Intent = "sale",
+                Transactions = new List<Transaction>()
+                {
+                    new Transaction()
+                    {
+                        Amount = new Amount()
+                        {
+                            Total = total.ToString(),
+                            Currency = "USD",
+                            Details = new AmountDetails
+                            {
+                                Tax = "0",
+                                Shipping = "0",
+                                Subtotal = total.ToString()
+                            }
+                        },
+                        ItemList = itemList,
+                        Description = $"Invoice #{paypalOrderId}",
+                        InvoiceNumber = paypalOrderId.ToString()
+                    }
+                },
+                RedirectUrls = new RedirectUrls()
+                {
+                    CancelUrl = $"{hostName}/hoadons/CheckoutFail",
+                    ReturnUrl = $"{hostName}/hoadons/CheckoutSuccess"
+                },
+                Payer = new Payer()
+                {
+                    PaymentMethod = "paypal"
+                }
+            };
+
+            PaymentCreateRequest request = new PaymentCreateRequest();
+            request.RequestBody(payment);
+
+            try
+            {
+                var response = await client.Execute(request);
+                var statusCode = response.StatusCode;
+                Payment result = response.Result<Payment>();
+
+                var links = result.Links.GetEnumerator();
+                string paypalRedirectUrl = null;
+                while (links.MoveNext())
+                {
+                    LinkDescriptionObject lnk = links.Current;
+                    if (lnk.Rel.ToLower().Trim().Equals("approval_url"))
+                    {
+                        //saving the payapalredirect URL to which user will be redirected for payment  
+                        paypalRedirectUrl = lnk.Href;
+                    }
+                }
+
+                return Redirect(paypalRedirectUrl);
+            }
+            catch (HttpException httpException)
+            {
+                var statusCode = httpException.StatusCode;
+                var debugId = httpException.Headers.GetValues("PayPal-Debug-Id").FirstOrDefault();
+
+                //Process when Checkout with Paypal fails
+                return Redirect("/hoadons/CheckoutFail");
+            }
+
+        }
+
+        public IActionResult CheckoutFail()
+        {
+            return View();
+        }
+        public IActionResult CheckoutSuccess()
+        {
+            return View();
         }
 
         // GET: HoaDons
